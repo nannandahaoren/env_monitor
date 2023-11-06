9380c1bf21216a597ae6e370f76a47be001873f4
张仲楠 <2577227831@qq.com>
2023-11-06 19:20:06 +0800

    修改格式

---
+++ ESP32_device/main.py
@@ -0,0 +1,125 @@
+import time
+import network
+import ujson
+from simple import MQTTClient  # 上面的步骤就是在下载umqtt.simple到ESP32设备
+from machine import Pin
+from wifi import do_connect
+from umqtt import sub_cb, on_connect, on_message
+import array as arr
+from kalman_average import tempAverage, tempKalman
+import struct
+from machine import Pin, SoftI2C
+from BME280 import BME280
+
+MQTT_CLIENT = None
+LED = None
+
+# 发布订阅模式需要确定订阅什么主题
+pub_topic = "temperature"
+sub_topic = "sensor"
+# 下面根据实际情况进行修改
+
+
+# 维护一个临时数组，用于存储温度值
+temp_array = arr.array("d")
+# 维护一个存储数组，用于存储到数据库中
+dataset_temp = arr.array("d")
+
+dataset_time = arr.array("d")
+
+# ESP32 - Pin assignment
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=10000)
+
+# 存放临时数组的平均值
+T = 0
+
+
+
+def main():
+    global MQTT_CLIENT, LED, temp_array, dataset_temp, dataset_time,T, i2c
+    global pub_topic, sub_topic
+
+    # 1.链接WiFi
+    do_connect()
+    # 创建mqtt客户端
+    # 第一个参数 客户端的名字
+    # 第二个参数mqtt服务端的ip地址
+    # 第三个参数mqtt服务端的端口号
+    # 第四个参数mqtt服务端的管理员账号
+    # 第五个参数mqtt服务端的管理员密码
+    # 第六个参数默认设置
+    MQTT_CLIENT = MQTTClient(
+        "ESP32_1", "101.43.132.163", 1883, "admin", "Lj112358", keepalive=60
+    )
+
+    print("connectted to mqtt_server !")
+
+    MQTT_CLIENT.set_callback(on_message)  # 设置回调函数
+    MQTT_CLIENT.connect()  # 建立连接
+    MQTT_CLIENT.subscribe(sub_topic)
+    
+
+    time.sleep(1)
+    dataset_temp = []
+    dataset_time = []
+    temp_array = []
+    
+
+    while True:
+        bme = BME280(i2c=i2c)
+        current_time = time.time()
+        
+        # 下方t是读取的温度值,测量值
+        str_t = bme.temperature  # 获取带有字符串C的字符串形式的温度
+        str_t = str_t.replace("C", "")  # 去掉带有字符串C的字符串形式的温度
+        t = float(str_t)  # 将字符串形式的温度转化成浮点数形式
+        temp_array.append(t)  # 将温度值添加到临时数组中
+        # hum = bme.humidity
+        # pres = bme.pressure
+        # uncomment for temperature in Fahrenheit
+        # temp = (bme.read_temperature()/100) * (9/5) + 32
+        # temp = str(round(temp, 2)) + 'F'
+
+        if len(temp_array) == 8:
+            T = tempAverage(temp_array)  # 当临时数组的元素个数为5个时，求平均
+            print("T: ", T)
+            length = len(dataset_temp)  # 计算dataset_temp的长度
+
+            if length == 0:
+                dataset_temp.append(T)
+                dataset_time.append(current_time)
+            else:
+                length -= 1
+                temp = dataset_temp[length]
+                dataT = tempKalman(temp, T)
+                round_number = round(dataT, 1)
+                print("-----------------------------")
+                print("temprature after kalmanfilter is ", round_number)
+                dataset_temp.append(dataT)
+                dataset_time.append(current_time)
+                derta_time = current_time - dataset_time[length]
+                str_msg = str(derta_time)+','+str(round_number)
+                
+                
+                # msg = struct.pack("f", round_number)
+                msg = struct.pack("12s", str_msg.encode('utf-8'))
+                MQTT_CLIENT.publish(pub_topic, msg)
+            temp_array = []
+
+        MQTT_CLIENT.check_msg()
+
+        # msg = b"22.78"
+        # 发送自动配置MQTT服务的数据包
+        if len(dataset_temp) == 64:
+            dataset_temp = []
+            dataset_time = []
+            temp_array= []
+            
+
+        time.sleep(0.36)
+
+
+if __name__ == "__main__":
+    
+    main()
+
--- Ubuntu_server/subData.py
+++ Ubuntu_server/subData.py
@@ -4,6 +4,7 @@ import time
 import struct
 import csv
 from pymysql import *
+import pymysql
 
 csv_file = "data.csv"
 broker = "101.43.132.163"
@@ -13,7 +14,7 @@ username = "emqx"
 password = "public"
 msg = b"hello_esp32"
 # 数据库device_info中表的名字，对应唯一的SN号码
-dataBaseName = "device_1_SN1699081164022"
+dataBaseName = "device_SN0011699081164022"
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -27,17 +28,33 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 # subscribe 订阅到相应的主题后，打印该主题和对应的消息，消息是字节的形式，需要转化为字符串或者数字型
 def on_message(client, userdata, msg, Cursor, conn):
-    float_number = struct.unpack("f", msg.payload)[0]
-    print(msg.topic + " " + str(float_number))
-    # 将收到的信息写入到数据库,构造执行语句
-    mysql_command = "insert into" + " " + dataBaseName + " "
-    mysql_command = mysql_command + "values(" + "now()" + "," + str(float_number) + ")"
+    global derta, start_time, end_time
+    float_number = struct.unpack("12s", msg.payload)[0]
+    msg = float_number.decode("utf-8")
+    print(msg)
 
-    # 执行insert语句，并返回受影响的行数：添加一条数据
-    count = Cursor.execute(mysql_command)
-    # 提交之前的操作，如果之前已经之执行过多次的execute，那么就都进行提交
-    # 必须提交这个才能使上面的语句生效
-    conn.commit()
+    # print(msg.topic + " " + str(float_number))
+    # 将收到的信息写入到数据库,构造执行语句
+
+    # insert into device_1_SN0011699081164022 values(now(),'28,56,435')
+    # '28,56,435',第一个28是温度，第二个56是湿度，第三个435是二氧化碳浓度值
+    # 在最前面插入时间间隔
+    mysql_command = "insert into" + " " + dataBaseName + " "
+    mysql_command = (
+        mysql_command + "values(" + "0" + "," + "now()" + "," + "'" + msg + "'" + ")"
+    )
+    print(mysql_command)
+
+    try:
+        # 执行insert语句，并返回受影响的行数：添加一条数据
+        count = Cursor.execute(mysql_command)
+        # 提交之前的操作，如果之前已经之执行过多次的execute，那么就都进行提交
+        # 必须提交这个才能使上面的语句生效
+        conn.commit()
+        print("已将数据写入数据库！")
+    except pymysql.err.ProgrammingError:
+        print("数据库不存在！")
+        pass
 
 
 def createCursor():
